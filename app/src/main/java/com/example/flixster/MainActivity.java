fc828f288b1fbce0155b49471c28eec3a3969118
Maya Epps <mepps@fb.com>
2020-06-24 11:12:50 -0700

    Add API call/networking and parsing JSON

--- app/src/main/java/com/example/flixster/MainActivity.java
+++ app/src/main/java/com/example/flixster/MainActivity.java
@@ -3,16 +3,25 @@ package com.example.flixster;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 
 import com.codepath.asynchttpclient.AsyncHttpClient;
 import com.codepath.asynchttpclient.callback.JsonHttpResponseHandler;
+import com.example.flixster.models.Movie;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+
+import java.util.List;
 
 import okhttp3.Headers;
 
 public class MainActivity extends AppCompatActivity {
 
-    public static final String NOW_PLAYING_URL = "https://api.themoviedb.org/3/movie/now_playing?api_key=a07e22bc18f5cb106bfe4cc1f83ad8ed";
+    public static final String NOW_PLAYING_URL = "https://api.themoviedb.org/3/movie/now_playing?api_key=34f20dcfe993a740e40b25ef2a25e3be";
     public static final String TAG = "MainActivity";
+
+    List<Movie> movieList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,6 +29,25 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         AsyncHttpClient client = new AsyncHttpClient();
+        client.get(NOW_PLAYING_URL, new JsonHttpResponseHandler() {
+            @Override
+            public void onSuccess(int statusCode, Headers headers, JSON json) {
+                Log.d(TAG, "onSuccess");
+                try {
+                    JSONArray results = json.jsonObject.getJSONArray("results");
+                    Log.i(TAG, "Results: " + results.toString());
+                    movieList = Movie.fromJSONArray(results);
+                    Log.i(TAG, "Movies: " + movieList.size());
+                } catch (JSONException e) {
+                    Log.e(TAG, "Hit JSON exception", e);
+                }
+            }
+
+            @Override
+            public void onFailure(int statusCode, Headers headers, String response, Throwable throwable) {
+                Log.d(TAG, "onFailure");
+            }
+        });
 
     }
 }
---
+++ app/src/main/java/com/example/flixster/models/Movie.java
@@ -0,0 +1,44 @@
+package com.example.flixster.models;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Movie {
+
+    String posterPath;
+    String title;
+    String overview;
+
+    public Movie(JSONObject json) throws JSONException {
+
+        posterPath = json.getString("poster_path");
+        title = json.getString("title");
+        overview = json.getString("overview");
+
+    }
+
+    public static List<Movie> fromJSONArray(JSONArray movieJSONArray) throws JSONException {
+        List<Movie> movies = new ArrayList<>();
+        for(int i = 0; i < movieJSONArray.length(); i++) {
+            movies.add(new Movie(movieJSONArray.getJSONObject(i)));
+        }
+
+        return movies;
+    }
+
+    public String getPosterPath() {
+        return String.format("https://image.tmdb.org/t/p/w342/%s", posterPath);
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getOverview() {
+        return overview;
+    }
+}
